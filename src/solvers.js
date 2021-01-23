5f3f175fb36a25cac4bec4189415944345aba7ea
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-01-23 12:34:10 -0800

    Complete Bare Minimum Requirements

--- src/solvers.js
+++ src/solvers.js
@@ -56,25 +56,53 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var board = new Board({'n': n});
-  var solution = board.rows();
-  //create var for representing rows of border
-  for (var i = 0; i < solution.length; i++) {
-    for (var j = 0; j < solution.length; j++) {
-      solution[i][j] = 1;
-      if (board.hasAnyQueensConflicts()) {
-        solution[i][j] = 0;
-      }
-    }
+  var board = new Board({n});
+  var currentBoard = board.rows();
+  var solution = undefined;
+
+  if (n === 2 || n === 3) {
+    return currentBoard;
   }
+
+  var checkBoard = function(colIndex) {
+    if (colIndex === currentBoard.length) { //do not have a conflict
+      solution = currentBoard;
+      return;
+    }
+    for (var i = 0; i < currentBoard.length; i++) {
+      currentBoard[i][colIndex] = 1;
+      if (!board.hasAnyQueenConflictsOn(i, colIndex)) {
+        checkBoard(colIndex + 1);
+      }
+      if (solution !== undefined) {
+        break;
+      }
+      currentBoard[i][colIndex] = 0;
+    }
+  };
+  checkBoard(0);
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
+  var board = new Board({n});
+  var solutionBoard = board.rows();
+  var solutionCount = 0;
+  var countQueens = function(colIndex) {
+    if (colIndex === solutionBoard.length) {
+      solutionCount++;
+    }
+    for (var i = 0; i < solutionBoard.length; i++) {
+      solutionBoard[i][colIndex] = 1;
+      if (!board.hasAnyQueensConflicts()) {
+        countQueens(colIndex + 1);
+      }
+      solutionBoard[i][colIndex] = 0;
+    }
+  };
+  countQueens(0);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
